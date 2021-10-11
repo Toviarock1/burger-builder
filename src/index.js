d0d9992b944d9873566ac99f90ceb04bba97944f
simon adama <simontoviaadama@gmail.com>
2021-10-11 12:19:57 +0100

    finish redux-saga setup

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 import dashboardReducer from './store/reducers/dashboard';
+import { logoutSaga } from './store/sagas/auth';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from '@redux-saga/core';
 
 const composeEnchancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
@@ -21,9 +23,13 @@ const rootReducer = combineReducers({
     dashboard: dashboardReducer
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(rootReducer,  composeEnchancers(
-	applyMiddleware(thunk)
+	applyMiddleware(thunk, sagaMiddleware)
 ));
+
+sagaMiddleware.run(logoutSaga);
 
 const app = (
 
